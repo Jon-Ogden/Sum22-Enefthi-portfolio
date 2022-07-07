43119c14a7d58648b13042ebe49bdbeea08264d7
Jon_Ogden <jogden1863@gmail.com>
2022-07-07 17:28:29 -0500

    models and controllers generated

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_20_203217) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_07_220649) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "nfts", force: :cascade do |t|
+    t.float "price"
+    t.string "image"
+    t.text "description"
+    t.bigint "user_id", null: false
+    t.boolean "for_sale"
+    t.datetime "sale_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_nfts_on_user_id"
+  end
+
+  create_table "sales", force: :cascade do |t|
+    t.bigint "nft_id", null: false
+    t.integer "sold_by"
+    t.integer "purchased_by"
+    t.float "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["nft_id"], name: "index_sales_on_nft_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "provider", default: "email", null: false
@@ -44,4 +66,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_20_203217) do
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
+  add_foreign_key "nfts", "users"
+  add_foreign_key "sales", "nfts"
 end
